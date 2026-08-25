8ab1696bac2d772b70282cdf0b98c54e2e813d77
zeekpowta1-spec <zeekpowta1@gmail.com>
2026-08-25 13:58:09 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "ua.mypershymillion"
         minSdk = 23
         targetSdk = 36
-        versionCode = 15
+        versionCode = 14
         versionName = "1.3"
     }
 
@@ -32,13 +32,15 @@ android {
 
 dependencies {
     val composeBom = platform("androidx.compose:compose-bom:2026.06.00")
+
     implementation(composeBom)
-    androidTestImplementation(composeBom)
 
     implementation("androidx.activity:activity-compose:1.13.0")
+
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
+
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
